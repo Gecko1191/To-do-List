c429eb3b07311b1f07d35d1884950861cde622d2
D066024 <Tobias.Koehler01@sap.com>
2024-05-30 14:21:41 +0200

    docs: add some comments for the userStore and component

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -5,13 +5,17 @@ import type { user } from "@/components/UserMenu/types";
 /*
 The store for the Current User
 
-@returns the current user
+@returns the current user and getUser function
  */
 export const useUserStore = defineStore("user", () => {
   const user = ref<user>();
 
   async function getUser() {
-    //in a real word example we could call something like users/me to get the current user
+    /*
+      in a real word example we could call something like users/me
+      to get the current user in an sso context
+     */
+
     fetch(`https://jsonplaceholder.typicode.com/users/${1}`)
       .then((response) => response.json())
       .then((json) => (user.value = json));
