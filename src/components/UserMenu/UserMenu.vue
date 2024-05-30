9c76bedfa1aa705272106e091e019d9a44153594
D066024 <Tobias.Koehler01@sap.com>
2024-05-30 14:11:55 +0200

    feat: add user store and component wrapper to load the user

---
+++ src/components/UserMenu/UserMenu.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import { OnyxUserMenu, type SelectOption } from "sit-onyx";
+import { useUserStore } from "@/stores/user";
+import logout from "@sit-onyx/icons/tool-table.svg?raw";
+import settings from "@sit-onyx/icons/settings.svg?raw";
+
+const userStore = useUserStore();
+userStore.getUser();
+
+const userMenuOptions = [
+  { value: "/settings", label: "Settings", icon: settings },
+  { value: "logout", label: "Logout", icon: logout, color: "danger" },
+] satisfies SelectOption[];
+
+//TODO: more user specific functions could be added here
+</script>
+
+<template>
+  <OnyxUserMenu
+    :username="userStore?.user?.name ?? 'could not find user name'"
+    :options="userMenuOptions"
+  />
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/stores/user.ts
@@ -0,0 +1,21 @@
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
+import type { user } from "@/components/UserMenu/types";
+
+/*
+The store for the Current User
+
+@returns the current user
+ */
+export const useUserStore = defineStore("user", () => {
+  const user = ref<user>();
+
+  async function getUser() {
+    //in a real word example we could call something like users/me to get the current user
+    fetch(`https://jsonplaceholder.typicode.com/users/${1}`)
+      .then((response) => response.json())
+      .then((json) => (user.value = json));
+  }
+
+  return { user, getUser };
+});
